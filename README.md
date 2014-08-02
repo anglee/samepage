81a30a94bd9d27e7339610e8edaa9a3e5edbbeaf
anglee <ang.lee@gmail.com>
2014-08-02 13:01:03 -0400

    Basic express GET & POST

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 Samepage
 =======
-Are we on the Samepage?
+Start
+
+```
+$ node samepage.js
+connect to: http://localhost:3000
+```
+
+curl
+
+```
+$ curl http://localhost:3000
+Hello Samepage
+$ curl http://localhost:3000/page?foo=bar
+req.method = GET
+req.url =/page?foo=bar
+req.query = {"foo":"bar"}
+req.body = {}
+req.params = {}
+$ curl http://localhost:3000/page -d foo=bar
+req.method = POST
+req.url =/page
+req.query = {}
+req.body = {"foo":"bar"}
+req.params = {}
+$ curl http://localhost:3000/page/1
+req.method = GET
+req.url =/page/1
+req.query = {}
+req.body = {}
+req.params = {"id":"1"}
+```
+
--- samepage.js
+++ samepage.js
@@ -1,9 +1,34 @@
 var express = require("express");
-
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var port = 3000;
 var app = express();
 
-app.get('/', function (req, res) {
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// parse application/vnd.api+json as json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }))
+
+app.route('/').get(function (req, res) {
   res.send("Hello Samepage");
 });
 
-app.listen(3000);
+var handler = function (req, res) {
+  res.write("req.method = " + req.method + "\n");
+  res.write("req.url =" + req.url + "\n");
+  res.write("req.query = " + JSON.stringify(req.query) + "\n");
+  res.write("req.body = " + JSON.stringify(req.body) + "\n");
+  res.write("req.params = " + JSON.stringify(req.params) + "\n");
+  res.end();
+};
+
+app.route('/page').all(handler);
+app.route('/page/:id').all(handler);
+
+app.listen(port);
+console.log("connect to: http://localhost:" + port);
